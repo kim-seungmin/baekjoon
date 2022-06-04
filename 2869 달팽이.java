255094cef31eb946ab27cad64221a6d84825f2c5
kim-seungmin <54826101+kim-seungmin@users.noreply.github.com>
2022-06-05 00:14:27 +0900

    Create 2869 달팽이.java

---
+++ 2869 달팽이.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Main {
+	public static void main(String[] args) throws IOException {		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] s = br.readLine().split(" ");
+		int i=0;
+		if((Integer.parseInt(s[2])-Integer.parseInt(s[1]))%(Integer.parseInt(s[0])-Integer.parseInt(s[1]))!=0){
+			i++;
+		}
+		System.out.println((Integer.parseInt(s[2])-Integer.parseInt(s[1]))/(Integer.parseInt(s[0])-Integer.parseInt(s[1]))+i);
+	}
+}
