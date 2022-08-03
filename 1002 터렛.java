6a10e1e860b7464f08c21fd712611066d4619bdd
kim-seungmin <54826101+kim-seungmin@users.noreply.github.com>
2022-08-03 17:33:12 +0900

    Create 1002 터렛.java

---
+++ 1002 터렛.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+		for(int i=0;i<n;i++) {	
+
+			st = new StringTokenizer(br.readLine());
+			
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int r1 = Integer.parseInt(st.nextToken());
+
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			int r2 = Integer.parseInt(st.nextToken());
+			
+
+			
+			int distance_pow = (int)(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+			
+
+			if(x1 == x2 && y1 == y2 && r1 == r2) {
+				System.out.println("-1");
+			}
+			else if(distance_pow > Math.pow(r1 + r2, 2)||distance_pow < Math.pow(r2 - r1, 2)) {
+				System.out.println("0");
+			}
+
+			else if(distance_pow == Math.pow(r2 - r1, 2)||distance_pow == Math.pow(r1 + r2, 2)) {
+				System.out.println("1");
+			}			
+			else {
+				System.out.println("2");
+			}
+		}
+	}	
+}
